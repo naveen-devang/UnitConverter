c145b19984efe799cdff87c12423afdfff27e057
Naveen <90497956+naveen-devang@users.noreply.github.com>
2024-11-09 18:19:20 +0530

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Naveen on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
